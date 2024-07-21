d649ec24fcf71209da4e499633d74096beed222d
Wendel <wendelsouza.dev@gmail.com>
2024-07-21 13:43:49 -0300

    iniciando projeto

---
+++ app/Http/Controllers/FinancasController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FinancasController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('financas.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FinancasController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/financas', [FinancasController::class, 'index'])->name('financeiro.home');
+
+require __DIR__.'/auth.php';
