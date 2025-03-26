7ac446ed1e6746af3ff718f1a70beb084e3c220f
Wendel <wendelsouza.dev@gmail.com>
2025-03-25 22:51:10 -0300

    em breve page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\FinancasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('teste');
+    return view('embreve');
 });
 
 Route::get('/inicio', function () {
